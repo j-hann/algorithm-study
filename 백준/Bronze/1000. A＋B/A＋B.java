6309870478a329a516bb0f77da9a6a833d98571a
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-20 20:47:47 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //scanner 변수 선언, 초기화
+        Scanner sc = new Scanner(System.in);
+        
+        //첫번째 수 A
+        int a = sc.nextInt();
+        //두번째 수 B
+        int b = sc.nextInt();
+        
+        System.out.println(a + b);
+    }//main
+}//end class
