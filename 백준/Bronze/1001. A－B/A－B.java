0af4a00c1be09770dd260c30369c11687a874fc2
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-21 10:57:29 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //scanner 변수 선언, 초기화
+        Scanner sc = new Scanner(System.in);
+        //a입력받기
+        int a = sc.nextInt();
+        //b입력받기
+        int b = sc.nextInt();
+        //a-b출력
+        System.out.println(a-b);
+    }//main
+}//end class
