8b986fb47fa0061c9e60ffb92dfba31a6a06274b
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-21 09:27:30 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if(-50000 <= num1 && num2 <= 50000){
+            answer = num1 + num2;
+        }//if
+        return answer;
+    }//solution
+}//end class
