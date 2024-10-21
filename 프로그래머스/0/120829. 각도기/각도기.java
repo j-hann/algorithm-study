dc22246e047fe822add2c0a688f3a18795ce6869
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-22 01:22:14 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if(0 < angle && angle < 90){//예각
+               answer = 1;
+        } else if(angle == 90){//직각
+             answer = 2;
+        } else if(90 < angle && angle < 180){//둔각
+            answer = 3;
+        } else if(angle == 180){//평각
+            answer = 4;
+        }//if-else
+        return answer;
+    }//solution
+}//end class
