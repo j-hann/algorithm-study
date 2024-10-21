4f05aa1b3ca85f0f7553f4ea7fa122009176c9e7
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-21 11:05:13 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // " 를 표현하고 싶으면 \ 역슬래쉬를 붙여야 한다
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        
+    } // main
+    
+} // end class
