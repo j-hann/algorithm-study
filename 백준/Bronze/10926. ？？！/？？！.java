f980891aa397472cd782c062bbc8397c3c8a4c3c
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-21 10:59:07 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //id 입력받기
+        String id = sc.nextLine();
+        //마지막 문자열 변수 선언, 초기화
+        String str = "??!";
+        System.out.println(id + str);
+
+    }//main
+}//end class
