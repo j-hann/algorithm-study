c15e45b306f87717cf7c80d11c475f17e1250db8
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-21 11:01:31 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+ public class Main {
+     
+     public static void main(String[] args) {
+         
+         // 백슬래쉬(\) 문자는 이스케이프 문자이므로, 그 자체를 표현할 수 없다.
+         // 이때 백슬래쉬(\) 두번 사용해서 해결한다!
+         System.out.println("\\    /\\");   
+         System.out.println(" )  ( ')");
+         System.out.println("(  /  )");
+         System.out.println(" \\(__)|");
+         
+     } // main
+     
+ } // end class
