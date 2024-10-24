5046b8bacb2d3b2d74f3f3cb62593e0124b4b9ec
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-24 10:21:44 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0){
+            answer = "Even";
+        }else{
+            answer = "Odd";
+        }//if-else
+        
+        return answer;
+    }//solution
+}//end class
