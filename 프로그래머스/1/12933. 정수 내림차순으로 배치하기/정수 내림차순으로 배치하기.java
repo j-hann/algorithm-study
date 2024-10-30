e4514fff7c206f7c038f278939c2403dd2b6ae68
j-hann <86395588+j-hann@users.noreply.github.com>
2024-10-30 13:55:43 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.75 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 class Solution {
-    public long solution(long n) {
+   public long solution(long n) {
         //long 타입 값 -> String 타입으로 변환
         String number = String.valueOf(n);
         //문자열 하나씩 쪼개기
@@ -12,14 +12,10 @@ class Solution {
         
         //String 배열 내림차순 정렬
        Arrays.sort(numberSplit, Collections.reverseOrder());
-        //numberSplit 배열 -> String 타입으로 변환 후 합치기
-        StringBuilder stringbuilder = new StringBuilder();
-       	for(String string : numberSplit){
-       		stringbuilder.append(string);
-       	}//for
-        
+        //String 배열 -> String 타입 문자열로 결합
+        String numberString = String.join("", numberSplit);
         //answer 배열에 Long 타입으로 변환해서 넣기
-        answer = Long.parseLong(stringbuilder.toString());
+        answer = Long.parseLong(numberString);
         
         return answer;
     }//solution
