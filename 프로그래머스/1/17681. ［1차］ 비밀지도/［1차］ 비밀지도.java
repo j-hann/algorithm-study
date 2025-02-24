b1a9bf1c436abbe20e5b22d219c1fde11d3cfca3
j-hann <86395588+j-hann@users.noreply.github.com>
2025-02-24 16:32:30 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.60 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,6 +1,61 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+        
+        //n개 크기만큼 배열 생성
+        String[] firstMap = new String[n];
+        String[] secondMap = new String[n];
+        
+        //첫번째 지도 - 인덱스에 해당하는 정수를 이진 문자열로 변환
+        for(int i = 0; i < n; i++){
+            String firstBinaryString = Integer.toBinaryString(arr1[i]);
+            
+            //문자열 길이가 n보다 작을 경우 -> 공백을 0으로 변환
+            while(firstBinaryString.length() < n){
+                firstBinaryString = "0" + firstBinaryString;
+            }   
+            
+            //변환된 이진 문자열을 0이면 공백, 1이면 #으로 치환 후 저장
+            firstMap[i] = firstBinaryString
+                .replace('0', ' ')
+                .replace('1', '#');
+        }
+        
+        //두번째 지도 - 인덱스에 해당하는 정수를 이진 문자열로 변환
+        for(int i = 0; i < n; i++){
+            String secondBinaryString = Integer.toBinaryString(arr2[i]);
+            
+            //문자열 길이가 n보다 작을 경우 -> 공백을 0으로 변환
+            while(secondBinaryString.length() < n){
+                secondBinaryString = "0" + secondBinaryString;
+            }   
+            
+            //변환된 이진 문자열을 0이면 공백, 1이면 #으로 치환 후 저장
+            secondMap[i] = secondBinaryString
+                .replace('0', ' ')
+                .replace('1', '#');
+        }
+        
+        //최종 지도 만들기
+        for(int i = 0; i < n; i++){
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            //두 배열 중 인덱스에 #이 있으면 # 넣고, 아니면 공백 넣기
+            for(int j = 0; j < n; j++){
+                if(firstMap[i].charAt(j) == '#' || secondMap[i].charAt(j) == '#'){
+                    stringBuilder.append('#');
+                } else {
+                    //공백 넣기
+                    stringBuilder.append(' ');
+                }
+            }
+            
+            
+            answer[i] = stringBuilder.toString();
+        }
+        
         return answer;
     }
 }
