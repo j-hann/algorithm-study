32c07fe4d5cac310d5f2403b50875fb2a1bda468
j-hann <j.han77055@gmail.com>
2025-02-26 15:44:31 +0900

    🔧 Add : 모의고사 문제 수정 & README 수정
    add : 모의고사 문제 수정 & README 수정

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 
-# 🪐 Algorithm Study
+##  🪐 Algorithm Study
 > 알고리즘 풀고 기록하기 ✏️
 
-## 📌 사용 언어
-- Java
+## ✏️ 사용 언어
+### Java, MySQL
 
 ## 🚀 단계별 정리
 ### ⚾ Baekjoon 
-- [Bronze](https://github.com/j-hann/algorithm-study/tree/main/%EB%B0%B1%EC%A4%80/Bronze)
+### 🥉 [Bronze](https://github.com/j-hann/algorithm-study/tree/main/%EB%B0%B1%EC%A4%80/Bronze)
+<br>
+
 ### ⚽ programmers
-- [Lv.0](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/0)
-- [Lv.1](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/1)
-
-
-
+### 💿 [Lv.0](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/0)
+### 💿 [Lv.1](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/1)
+### 💿 [Lv.2](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/2)
+### 💿 [Lv.3](https://github.com/j-hann/algorithm-study/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/3)
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,62 +1,56 @@
 import java.util.*;
 
 class Solution {
-    
+
 	public int[] solution(int[] answers) {
-			int[] answer = {};
+		int[] answer = {};
 
-			int[] first = new int[] {1,2,3,4,5};
-			int[] second = new int[] {2,1,2,3,2,4,2,5};
-			int[] third = new int[] {3,3,1,1,2,2,4,4,5,5};
+		int[] first = new int[]{1, 2, 3, 4, 5};
+		int[] second = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
+		int[] third = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-			//정답 개수 카운트
-			int[] count = new int[3];
+		//정답 개수 카운트
+		int[] count = new int[3];
 
-			//가장 높은 점수 저장
-			int maxCount = 0;
-			List<Integer> topScore = new ArrayList<>();
+		//가장 높은 점수 저장
+		int maxCount = 0;
+		List<Integer> topScore = new ArrayList<>();
 
-			//정답 채점
-			for(int i = 0; i < answers.length; i++){
-				//1번 수포자 - 나머지 통해서 패턴 반복
-				int firstAnswer = first[i % first.length];
-				//2번 수포자
-				int secondAnswer = second[i % second.length];
-				//3번 수포자
-				int thirdAnswer = third[i % third.length];
-
-				if(answers[i] == firstAnswer){
-					count[0]++;
-				}
-				if(answers[i] == secondAnswer){
-					count[1]++;
-				}
-				if(answers[i] == thirdAnswer){
-					count[2]++;
-				}
+		//정답 채점
+		for (int i = 0; i < answers.length; i++) {
+			//나머지 통해서 패턴 반복 & 카운트
+			if (answers[i] == first[i % first.length]) {
+				count[0]++;
 			}
-
-			for(int i = 0; i < count.length; i++){
-				if(count[i] > maxCount){
-					//가장 높은 점수 저장
-					maxCount = count[i];
-					//기존 리스트 초기화
-					topScore.clear();
-					//새롭게 높은 점수 받은 사람 추가
-					topScore.add(i + 1);
-				} else if(count[i] == maxCount){
-					//기존의 가장 높은 점수랑 같으면 추가
-					topScore.add(i+1);
-				}
+			if (answers[i] == second[i % second.length]) {
+				count[1]++;
 			}
-
-			//리스트 -> int 배열로 변환
-			answer = new int[topScore.size()];
-        
-			for(int i = 0; i < topScore.size(); i++){
-				answer[i] = topScore.get(i);
+			if (answers[i] == third[i % third.length]) {
+				count[2]++;
 			}
-
-			return answer;
 		}
+
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] > maxCount) {
+				//가장 높은 점수 저장
+				maxCount = count[i];
+				//기존 리스트 초기화
+				topScore.clear();
+				//새롭게 높은 점수 받은 사람 추가
+				topScore.add(i + 1);
+			} else if (count[i] == maxCount) {
+				//기존의 가장 높은 점수랑 같으면 추가
+				topScore.add(i + 1);
+			}
+		}
+
+		//리스트 -> int 배열로 변환
+		answer = new int[topScore.size()];
+
+		for (int i = 0; i < topScore.size(); i++) {
+			answer[i] = topScore.get(i);
+		}
+
+		return answer;
 	}
+}
