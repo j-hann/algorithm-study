190a1de729a5d63efd6c1e8779060c3f859682f8
j-hann <86395588+j-hann@users.noreply.github.com>
2025-03-05 16:51:56 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,12 +1,19 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution{
+    public int solution(int n, int a, int b) {
         int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        //몇번째 라운드에서 만나는지 카운트
+        int count = 0;
+        
+        //a 참가자와 b 참가자가 같은 번호가 될 때까지 반복
+        while(a != b){
+            
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+                
+            count++;
+        }
+    
+        answer = count;
         return answer;
     }
 }
