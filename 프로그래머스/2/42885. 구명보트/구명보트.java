2ae8637646c52077d9eaaf586daa64d78c96ec6b
j-hann <86395588+j-hann@users.noreply.github.com>
2025-09-13 02:24:05 +0900

    [level 2] Title: 구명보트, Time: 10.30 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,31 +1,37 @@
-import java.util.*;
+//[접근 방법]
+
+//구명보트 최대 2명 탑승 가능, 무게는 limit 이하 
+//가장 무거운 사람이 가장 가벼운 사람이랑도 못타면 -> 혼자 타야함
+
+//그리디 알고리즘
+//사람 오름차순 정렬
+//가장 작은 수랑 큰 숫자를 짝지어서 조건 만족 시키는지 확인, 두 합이 limit보다 작은지 체크
+//투포인터 방식으로 양쪽에서 인덱스 이동하면서 체크, 인덱스가 같아질 때까지 카운트 하면서 반복
+//무게가 limit 이상이면 끝 인덱스 왼쪽으로 이동
+//limit보다 같거나 작으면 끝 인덱스 이동과 함께, 첫 인덱스도 오른쪽으로 이동  
+
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        //필요한 구명보트 개수
         int answer = 0;
+        int lightWeightIndex = 0;
+        int heavyWeightIndex = people.length-1;
         
-        //몸무게 정렬
         Arrays.sort(people);
-        //가장 가벼운 사람 -> 인덱스 왼쪽부터
-        int light = 0;
-        //가장 무거운 사람 -> 인덱스 오른쪽부터
-        int heavy = people.length - 1;
         
-        while(light <= heavy){
-            //두명 다 태울 수 있으면
-            if(people[light] + people[heavy] <= limit){
-                //가벼운 사람 탑승 -> 인덱스 오른쪽으로 이동
-                light++;
+        while(lightWeightIndex <= heavyWeightIndex){
+            
+            if(people[lightWeightIndex] +  people[heavyWeightIndex] <= limit){
+                lightWeightIndex++;
+                heavyWeightIndex--;
+            } else {
+                heavyWeightIndex--;
             }
             
-            //두명 몸무게 합이 limit보다 크면 무거운 사람 탑승 -> 인덱스 왼쪽으로 이동
-            heavy--;
-            //사용된 구명 보트 카운트
             answer++;
         }
-            
+
         return answer;
-        
     }
 }
