d328779b4bfe97a7bfff10949d13606f697bd91d
j-hann <j.han77055@gmail.com>
2025-09-13 02:53:38 +0900

    ➕  add : 프로그래머스 예산 문제
    - 프로그래머스 예산 문제

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,33 +1,35 @@
+//[접근 방법]
+
+//구명보트 최대 2명 탑승 가능, 무게는 limit 이하
+//가장 무거운 사람이 가장 가벼운 사람이랑도 못타면 -> 혼자 타야함
+
+//그리디 알고리즘
+//사람 오름차순 정렬
+//가장 작은 수랑 큰 숫자를 짝지어서 조건 만족 시키는지 확인, 두 합이 limit보다 작은지 체크
+//투포인터 방식으로 양쪽에서 인덱스 이동하면서 체크, 인덱스가 같아질 때까지 카운트 하면서 반복
+//무게가 limit 이상이면 끝 인덱스 왼쪽으로 이동
+//limit보다 같거나 작으면 첫 인덱스도 오른쪽으로 이동
+
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] d, int budget) {
+    public int solution(int[] people, int limit) {
         int answer = 0;
-        int count = 0;
-        //오름차순 정렬
-        Arrays.sort(d);
-        //부서 전체 예산 누적
-        int departmentBudgetSum = 0;
-            
-        for(int i = 0; i < d.length; i++){
-            //현재 부서 예산 신청 금액
-            int targetDepartment = d[i];
-            
-            //부서 예산 더하기
-            int targetDepartmentSum = departmentBudgetSum + targetDepartment;
+        int lightWeightIndex = 0;
+        int heavyWeightIndex = people.length-1;
 
-            //예산 초과시 종료
-            if(budget < targetDepartmentSum){
-                answer = count;        
-                break;
+        Arrays.sort(people);
+
+        while(lightWeightIndex <= heavyWeightIndex){
+
+            if(people[lightWeightIndex] +  people[heavyWeightIndex] <= limit){
+                lightWeightIndex++;
             }
-            
-            //누적합 갱신하기
-            departmentBudgetSum = targetDepartmentSum;
-            count++;
-            answer = count; 
+
+            heavyWeightIndex--;
+            answer++;
         }
-        
+
         return answer;
     }
 }
