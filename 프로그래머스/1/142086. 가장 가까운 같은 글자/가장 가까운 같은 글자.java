3c0119a6cb8e53809089e0cb80a8fb45cf80fd1b
j-hann <86395588+j-hann@users.noreply.github.com>
2025-09-17 00:12:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.49 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,25 +3,29 @@
 //count 해주면서 비교 인덱스와 기준 인덱스 문자열이 일치하면 배열에 숫자 값 넣기
 //일치하지 않을 경우 -1
 
+// - 시간이 오래 걸릴 경우  -> 이전 인덱스를 기억해두고 현재 인덱스랑 거리 계산해서 넣기
+// HashMap 사용하여 인덱스랑 문자열 기억
+
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        int count = 0;
+        Map<Character, Integer> saveIndex = new HashMap<Character, Integer>();
 
         for(int i = 0; i < s.length(); i++){
             char character = s.charAt(i);
-            count = 0;
-            answer[i] = -1;
             
-            for(int j = i-1; j >= 0; j--){
-                char compareIndex = s.charAt(j);
-                count++;
-                
-                if(character == compareIndex){
-                    answer[i] = count;
-                    break;
-                }
+            Integer previousIndex = saveIndex.get(character);
+            
+            if(previousIndex == null){
+                answer[i] = -1;    
+            } else {
+                answer[i] = i - previousIndex;
             }
+            
+            saveIndex.put(character, i);
         }
         
         return answer;
