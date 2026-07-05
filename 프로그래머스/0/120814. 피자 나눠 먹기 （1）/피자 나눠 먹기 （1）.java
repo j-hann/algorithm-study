ee5af50f22514c39ec3aca78d5c4f3b3a3e31875
j-hann <86395588+j-hann@users.noreply.github.com>
2026-07-05 18:37:19 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,9 +1,10 @@
+//정수 나눗셈 올림 공식 사용 (a + b - 1) / b
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        double pizzaCount = (double) n / 7;
-        answer = (int) Math.ceil(pizzaCount);
+        answer = (n + 6) / 7;
         
         return answer;
     }
