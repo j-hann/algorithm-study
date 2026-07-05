4cffceda702e68f138796e0944449726f26ba178
j-hann <86395588+j-hann@users.noreply.github.com>
2026-07-05 18:30:34 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,16 +1,9 @@
-// n / 7 = 1 -> 소수점 있다면 올림
-// 만약 7명 이하일 경우는 무조건 피자 1개
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        if(n <= 7){
-            answer = 1;            
-        } else {
-            double pizzaCount = (double) n / 7;
-            answer = (int) Math.ceil(pizzaCount);
-        }
+
+        double pizzaCount = (double) n / 7;
+        answer = (int) Math.ceil(pizzaCount);
         
         return answer;
     }
